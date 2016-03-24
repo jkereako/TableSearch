07ce7d19e7eea2630a5e858b3de327b063c9ee8c
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-03-23 22:15:34 -0400

    Add files

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  TableSearch
+//
+//  Created by Jeff Kereakoglow on 3/23/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
---
+++ Source/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableSearch
+//
+//  Created by Jeff Kereakoglow on 3/23/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
